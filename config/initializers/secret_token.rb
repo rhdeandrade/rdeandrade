7acab6dab47ec48a11a5577a5e1636e605ec7d94
Raphael de Andrade <raphael@simepar.br>
2012-10-18 09:37:50 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rdeandrade::Application.config.secret_token = '0ad1d0e3d99ecd03280786a84cccf4aea350131396631b8f3e683a47e1d153c6684e251792b5b49ce2e0a51cb12714f0fee492d751fd387a67ec52af66a17c87'
